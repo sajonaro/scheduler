ba4ff1fc49c3aecd325d1ad0ca05d40a066f0b92
sajonaro <alex.kunich@gmail.com>
2022-07-11 22:28:54 +0300

    added data file

---
+++ build.clj
@@ -0,0 +1,22 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'org/sch)
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s.jar" (name lib)))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version "0.0.1"
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file
+          :main 'scheduler.main}))
