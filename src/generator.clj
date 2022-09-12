2204a28b1a0fdb8001b29f4925685ba4415286cb
sajonaro <alex.kunich@gmail.com>
2022-09-12 20:12:24 +0300

    implemented first ETL method with Spectre lib

--- src/generator.clj
+++ src/generator.clj
@@ -1,5 +1,10 @@
-(ns generator)
+(ns generator
+  (:require [clojure.edn :as edn]
+            [com.rpl.specter :refer :all]))
 
+
+(defn in? [elm coll]
+  (some #(= % elm) coll))
 
 ;;;generate schedule based on data
 ;;;provided in curriculum
@@ -7,3 +12,20 @@
 (defn generate-schedule [curriculum]
   ;;;TBD
   (str (curriculum :subjs)))
+
+
+;;;find professors able to teach given subject and add them to subject
+(defn enrich-subjs-with-prof-ids [curriculum]
+  ;;;enrich each subjects with a list of
+  ;;;professors able to teach it
+  (transform [:subjs ALL ]
+    (fn [sbj]
+      (assoc sbj 
+             :profs
+             (select [:profs ALL #(in? (sbj :id) (% :subjs))] curriculum))) 
+        curriculum))
+
+
+;;;some tests
+(def data (edn/read-string (slurp "data/ex1.edn")))
+(enrich-subjs-with-prof-ids data)
